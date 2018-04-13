d540e8c223408f07fe5d29baf4273782281df772
NilmergOmino <nilmerg.omino@gmail.com>
2018-04-13 13:17:06 +0200

    main functionality added

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -80,23 +80,129 @@ window.addEventListener('DOMContentLoaded', function () {
 var Query = {
     startPanel: document.getElementById('startPanel'),
     endPanel: document.getElementById('endPanel'),
+    form: document.getElementById('form'),
     questionnaireTitle: document.getElementById('questionnaireTitle'),
     buttonStart: document.getElementById('buttonStart'),
-    question: document.getElementById('question'),
     choicesContainer: document.getElementById('choicesContainer'),
     buttonBack: document.getElementById('buttonBack'),
     buttonNext: document.getElementById('buttonNext'),
+    buttonFinish: document.getElementById('buttonFinish'),
     progress: document.getElementById('progress'),
     init: function init() {
+        this.setTitle();
+        this.questionsArr = [];
+        // this.progress = 0;
         this.setQuestions();
-        this.setProgress();
+        this.setProgress(0);
+        this.radioItems = document.querySelectorAll('._js-radio');
+        this.currentQuestion = 0;
+        this.lastQuestion = this.questionsArr.length - 1;
+        this.radioItems.forEach(function (el) {
+            return el.addEventListener('click', function () {
+                var checkedItems = document.querySelectorAll('._js-radio:checked').length;
+                Query.setProgress(checkedItems);
+                if (checkedItems == Query.lastQuestion + 1) {
+                    Query.showButtonFinish();
+                }
+            });
+        });
         this.buttonStart.addEventListener('click', function () {
-            console.log('aa');
+            Query.hide(Query.startPanel);
+            Query.show(Query.form);
+            Query.showQuestion(0);
+        });
+        this.buttonBack.addEventListener('click', function (event) {
+            Query.showQuestion(-1);
+            event.preventDefault();
+        });
+        this.buttonNext.addEventListener('click', function (event) {
+            Query.showQuestion(1);
+            event.preventDefault();
         });
     },
-    setQuestions: function setQuestions() {},
-    setProgress: function setProgress() {}
+    setTitle: function setTitle() {
+        this.questionnaireTitle.textContent = Questions.title;
+    },
+    setQuestions: function setQuestions() {
+        var fragment = document.createDocumentFragment();
 
+        var _loop = function _loop(key) {
+            if (Questions.hasOwnProperty(key)) {
+                if (key != 'title') {
+                    var choicesItem = document.createElement('div');
+                    choicesItem.classList.add('form_choicesItem', 'choicesItem', '-hidden');
+                    var questionTitle = document.createElement('h3');
+                    questionTitle.classList.add('form_question');
+                    questionTitle.textContent = Questions[key].question;
+                    choicesItem.appendChild(questionTitle);
+
+                    fragment.appendChild(choicesItem);
+                    Questions[key].choices.forEach(function (el, index) {
+                        var choiceBox = document.createElement('div');
+                        choiceBox.classList.add('choicesItem_box');
+                        choicesItem.appendChild(choiceBox);
+                        var id = key + "-" + index;
+
+                        choiceBox.innerHTML = '<input type="radio" name="' + key + '" class="choicesItem_radio _js-radio" value="' + el + '" id="' + id + '">';
+
+                        choiceBox.innerHTML += '<label for="' + id + '" class="choicesItem_label">' + el + '</label>';
+                    });
+                    Query.questionsArr.push(choicesItem);
+                }
+            }
+        };
+
+        for (var key in Questions) {
+            _loop(key);
+        }
+        this.choicesContainer.appendChild(fragment);
+    },
+    setProgress: function setProgress(value) {
+        var max = Query.lastQuestion + 1;
+        var current = value / max * 100;
+        Query.progress.style.width = current + '%';
+    },
+    hide: function hide(el) {
+        el.classList.add('-hidden');
+    },
+    show: function show(el) {
+        el.classList.remove('-hidden');
+    },
+    showQuestion: function showQuestion(direction) {
+        Query.questionsArr.forEach(function (el) {
+            return Query.hide(el);
+        });
+        Query.currentQuestion += direction;
+        if (Query.currentQuestion <= 0) {
+            Query.currentQuestion = 0;
+            Query.disableButton(Query.buttonBack);
+            Query.enableButton(Query.buttonNext);
+        } else if (Query.currentQuestion >= Query.lastQuestion) {
+            Query.currentQuestion = Query.lastQuestion;
+            Query.disableButton(Query.buttonNext);
+            Query.enableButton(Query.buttonBack);
+        } else {
+            Query.enableButton(Query.buttonNext);
+            Query.enableButton(Query.buttonBack);
+        }
+        var element = Query.questionsArr[Query.currentQuestion];
+        Query.show(element);
+    },
+    disableButton: function disableButton(el) {
+        el.classList.add('-disabled');
+    },
+    enableButton: function enableButton(el) {
+        el.classList.remove('-disabled');
+    },
+    showButtonFinish: function showButtonFinish() {
+        this.show(this.buttonFinish);
+        this.buttonFinish.addEventListener('click', function (event) {
+            Query.hide(Query.form);
+            Query.show(Query.endPanel);
+            event.preventDefault();
+            // sending all answers from filled form (database needed)
+        });
+    }
 };
 
 /***/ }),
@@ -109,7 +215,7 @@ var Query = {
 /* 2 */
 /***/ (function(module, exports) {
 
-module.exports = {"name":"Questionnaire about blabla.","q1":{"question":"blablabla?","choices":["aa","bb","cc"]},"q2":{"question":"blablabla?","choices":["dd","ee","ff"]}}
+module.exports = {"title":"Questionnaire about blabla.","q1":{"question":"blablabla?","choices":["aa","bb","cc"]},"q2":{"question":"blabla?","choices":["dd","ee","ff"]},"q3":{"question":"blablaxx?","choices":["dgag","agaw","ff","agqawet"]}}
 
 /***/ })
 /******/ ]);
--- src/js/app.js
+++ src/js/app.js
@@ -8,25 +8,120 @@ window.addEventListener('DOMContentLoaded', () =>{
 const Query = {
     startPanel: document.getElementById('startPanel'),
     endPanel: document.getElementById('endPanel'),
+    form: document.getElementById('form'),
     questionnaireTitle: document.getElementById('questionnaireTitle'),
     buttonStart: document.getElementById('buttonStart'),
-    question: document.getElementById('question'),
     choicesContainer: document.getElementById('choicesContainer'),
     buttonBack: document.getElementById('buttonBack'),
     buttonNext: document.getElementById('buttonNext'),
+    buttonFinish: document.getElementById('buttonFinish'),
     progress: document.getElementById('progress'),
     init: function(){
+        this.setTitle();
+        this.questionsArr = [];
+        // this.progress = 0;
         this.setQuestions();
-        this.setProgress();
+        this.setProgress(0);
+        this.radioItems = document.querySelectorAll('._js-radio');
+        this.currentQuestion = 0;
+        this.lastQuestion = this.questionsArr.length-1;
+        this.radioItems.forEach(el=>el.addEventListener('click', ()=>{
+            let checkedItems = document.querySelectorAll('._js-radio:checked').length;
+            Query.setProgress(checkedItems);
+            if(checkedItems == Query.lastQuestion+1){
+                Query.showButtonFinish();
+            }
+        }))
         this.buttonStart.addEventListener('click', ()=>{
-            console.log('aa');
+            Query.hide(Query.startPanel);
+            Query.show(Query.form);
+            Query.showQuestion(0);
+        })
+        this.buttonBack.addEventListener('click', (event)=>{
+            Query.showQuestion(-1);
+            event.preventDefault();
+        })
+        this.buttonNext.addEventListener('click', (event)=>{
+            Query.showQuestion(1);
+            event.preventDefault();
         })
     },
-    setQuestions: function(){
-
+    setTitle: function(){
+        this.questionnaireTitle.textContent = Questions.title;
     },
-    setProgress: function(){
+    setQuestions: function(){
+        let fragment = document.createDocumentFragment();
+        for(let key in Questions){
+            if(Questions.hasOwnProperty(key)){
+                if(key != 'title'){
+                    let choicesItem = document.createElement('div');
+                    choicesItem.classList.add('form_choicesItem', 'choicesItem', '-hidden');
+                    let questionTitle = document.createElement('h3');
+                    questionTitle.classList.add('form_question');
+                    questionTitle.textContent = Questions[key].question;
+                    choicesItem.appendChild(questionTitle);
 
+                    fragment.appendChild(choicesItem);
+                    Questions[key].choices.forEach((el,index)=>{
+                        let choiceBox = document.createElement('div');
+                        choiceBox.classList.add('choicesItem_box');
+                        choicesItem.appendChild(choiceBox);
+                        let id = key+"-"+index;
+
+                        choiceBox.innerHTML = '<input type="radio" name="'+key+'" class="choicesItem_radio _js-radio" value="'+el+'" id="'+id+'">';
+
+                        choiceBox.innerHTML += '<label for="'+id+'" class="choicesItem_label">'+el+'</label>';
+                    })
+                    Query.questionsArr.push(choicesItem);
+                }
+            }
+        }
+        this.choicesContainer.appendChild(fragment);
+    },
+    setProgress: function(value){
+        let max = Query.lastQuestion+1;
+        let current = value/max*100;
+        Query.progress.style.width = current+'%';
+    },
+    hide: function(el){
+        el.classList.add('-hidden');
+    },
+    show: function(el){
+        el.classList.remove('-hidden');
+    },
+    showQuestion: function(direction){
+        Query.questionsArr.forEach(el=>Query.hide(el));
+        Query.currentQuestion+=direction;
+        if(Query.currentQuestion <= 0){
+            Query.currentQuestion = 0;
+            Query.disableButton(Query.buttonBack);
+            Query.enableButton(Query.buttonNext);
+        }
+        else if(Query.currentQuestion >= Query.lastQuestion){
+            Query.currentQuestion = Query.lastQuestion;
+            Query.disableButton(Query.buttonNext);
+            Query.enableButton(Query.buttonBack);
+        }
+        else {
+            Query.enableButton(Query.buttonNext);
+            Query.enableButton(Query.buttonBack);
+        }
+        let element = Query.questionsArr[Query.currentQuestion];
+        Query.show(element);
+    },
+    disableButton: function(el){
+        el.classList.add('-disabled');
+    },
+    enableButton: function(el){
+        el.classList.remove('-disabled');
+    },
+    showButtonFinish: function(){
+        this.show(this.buttonFinish);
+        this.buttonFinish.addEventListener('click',(event)=>{
+            Query.hide(Query.form);
+            Query.show(Query.endPanel);
+            event.preventDefault();
+            // sending all answers from filled form (database needed)
+        })
     }
-
 }
